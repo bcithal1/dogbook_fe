95503de08800a4ffa3ab237b128bd7d5ff569315
iustina.istrati@mutualofomaha.com <iustina.istrati@mutualofomaha.com>
2023-03-07 20:18:47 -0600

    4th components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,20 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { useState } from "react";
 
 export default function Home() {
-  const {data: session } = useSession();
+  const { data: session } = useSession();
   const [name, setName] = useState(session?.user.name);
-  
+
   const doSubmit = () => {
+    // eslint-disable-next-line max-len
     // TODO(Trystan): Make this an axios instance. So that every use does not have to inject the header.
-    axios.put(`http://localhost:8080/api/v1/users/${session?.user.id}`, {...session.user, name}, {
-      headers: {Authorization: `Bearer ${session.accessToken}`}
-    });
-  }
+    axios.put(
+      `http://localhost:8080/api/v1/users/${session?.user.id}`,
+      { ...session.user, name },
+      {
+        headers: { Authorization: `Bearer ${session.accessToken}` },
+      }
+    );
+  };
 
   if (session) {
     return (
@@ -19,7 +24,11 @@ export default function Home() {
         Signed in as {session.user.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
         <form onSubmit={doSubmit}>
-          <input type="text" placeholder={session.user.name} onChange={(e) => setName(e.target.value)}/>
+          <input
+            type="text"
+            placeholder={session.user.name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <button type="submit">Update name</button>
         </form>
       </>
