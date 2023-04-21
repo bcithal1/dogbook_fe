ba62820d352e004071ee8ffe427e4729296b7b76
Brian <bcithal1@gmail.com>
2023-04-20 21:05:49 -0500

    weew

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -16,12 +16,14 @@ import {
   Portal,
   Link,
   Spinner,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 
 function PuppyCardSmall({ dog }: { dog: Dog }) {
   const { data: session } = useSession();
   const { status, data } = useGetDogPhoto(session?.accessToken, dog.id);
+  const imageHeight = useBreakpointValue({ base: "3px", md: "3px" });
   console.log({ data });
 
   if (status === "loading") {
@@ -40,12 +42,13 @@ function PuppyCardSmall({ dog }: { dog: Dog }) {
         textColor={"white"}
         alignContent={"center"}
       >
-        <Popover size={"3xl"}>
-          <PopoverTrigger>
+          
             <Box p={3} alignContent={"center"}>
               <Image
                 src={`data:image/png;base64, ${data}`}
                 alt={`Picture of ${dog.name}`}
+                minHeight={`${imageHeight}`}
+                // backgroundSize="fill"
                 rounded="18px"
                 boxShadow={
                   "0px 1px 18px -5px rgb(0 0 0 / 57%), 0 10px 10px -5px rgb(0 0 0 / 45%)"
@@ -63,46 +66,6 @@ function PuppyCardSmall({ dog }: { dog: Dog }) {
               </Text>
             </Box>
           </PopoverTrigger>
-          <Portal>
-            <PopoverContent
-              bg={"#F5F5F5"}
-              maxWidth={"123px"}
-              borderColor={"#886E58"}
-            >
-              <PopoverArrow bg={"#886E58"} />
-              <PopoverHeader>
-                <Text color={"#886E58"} fontWeight={"extrabold"}>
-                  {dog.name}
-                </Text>
-              </PopoverHeader>
-              <PopoverCloseButton />
-              <PopoverBody>
-                <Text>Sex: {dog.sex} </Text>
-                <Text>Age: {dog.age} </Text>
-              </PopoverBody>
-              <Link
-                href="/dog-profile"
-                style={{
-                  textDecoration: "none",
-                }}
-                alignContent="center"
-                colorScheme="#886E58"
-              >
-                <PopoverFooter>
-                  <Flex
-                    mt="1"
-                    justifyContent="center"
-                    alignContent="center"
-                    color={"#886E58"}
-                    fontWeight={"bold"}
-                  >
-                    <Text>Visit</Text>
-                  </Flex>
-                </PopoverFooter>
-              </Link>
-            </PopoverContent>
-          </Portal>
-        </Popover>
       </Box>
     </Flex>
   );
