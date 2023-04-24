6a4aac58f7f4b8dba18267061cc48d9002c45b96
Brian <bcithal1@gmail.com>
2023-04-24 13:30:56 -0500

    Started the formatting of the friendButton

--- src/components/Friends/FriendButton.tsx
+++ src/components/Friends/FriendButton.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import UserPage from "@/components/UserPage/UserPage";
+import { Friendship } from "@/types/friendship";
 import { Button } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 
@@ -13,8 +14,19 @@ export const FriendButton = () => {
   return <></>;
 };
 
-export const FriendCardFriendButton = () => {
+export const FriendCardFriendButton = ({
+  friendList,
+}: {
+  friendList: Friendship[];
+}) => {
   const { data: session } = useSession();
+  // session?.user.id
+
+  friendList.forEach((friend) => {
+    if (friend.secondaryUserId == session.user.id) {
+      return <Button onClick={handleClick}>Rm Friend</Button>;
+    }
+  });
 
   return <Button onClick={handleClick}>Button</Button>;
 
