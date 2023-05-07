cc7353efac236eb95b5b9041ae077f3990c38949
Brian <bcithal1@gmail.com>
2023-05-06 21:27:25 -0500

    Correctly routes dog profile pictures

--- src/queries/user.queries.ts
+++ src/queries/user.queries.ts
@@ -38,16 +38,28 @@ export function getUserById(accessToken: string, userId: number) {
   return { status, data };
 }
 
-export function getAllUser(accessToken: string){
-    const backendAPI = getAxiosBackend(accessToken);
-    const {status, data} = useQuery({
-        queryKey: ["getAllUser"],
-        queryFn: ()=>{
-            return backendAPI.get<User[]>(`/users`).then((res)=>res.data)
-        },
-        // make the query wait for accesstoken, !! is a short hand. !!accessToken turn it into a boolean
-        enabled:!!accessToken
-
-    })
-    return {status, data}
+export function getAllUser(accessToken: string) {
+  const backendAPI = getAxiosBackend(accessToken);
+  const { status, data } = useQuery({
+    queryKey: ["getAllUser"],
+    queryFn: () => {
+      return backendAPI.get<User[]>(`/users`).then((res) => res.data);
+    },
+    // make the query wait for accesstoken, !! is a short hand. !!accessToken turn it into a boolean
+    enabled: !!accessToken,
+  });
+  return { status, data };
 }
+
+export const useGetUserProfile = (accessToken: string, userId: number) => {
+  const backendAPI = getAxiosBackend(accessToken);
+  return useQuery<string>({
+    queryKey: ["getUserProfile", userId],
+    queryFn: () => {
+      return backendAPI.get(`/user/profile/${userId}`).then((response) => {
+        return response.data;
+      });
+    },
+    enabled: !!accessToken,
+  });
+};
