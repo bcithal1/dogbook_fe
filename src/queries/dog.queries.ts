258064ca7be760d86ba31ea3f59ec2fc3bb8aa3a
Brian <bcithal1@gmail.com>
2023-05-09 19:26:01 -0500

    Removed duplicate queries

--- src/queries/dog.queries.ts
+++ src/queries/dog.queries.ts
@@ -2,12 +2,9 @@ import { getAxiosBackend } from "@/api/api";
 import { Dog, DogProfile } from "@/types/dog";
 import { Breed } from "@/types/breed";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import dogProfile from "@/pages/dog-profile";
-import { DogProfile } from "@/types/dog-profile";
 import { BreedInfo } from "@/types/breed-info";
 import { DogOwner } from "@/types/dog-owner";
 import { User } from "@/types/user";
-import { useState } from "react";
 
 export const useCreateDog = (accessToken: string) => {
   const backendAPI = getAxiosBackend(accessToken);
@@ -33,15 +30,16 @@ export const useGetDogById = (accessToken: string, id: Dog["id"]) => {
 };
 
 export function useGetBreedList(accessToken: string) {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useQuery<Breed[]>({
-        queryKey: ["breeds"],
-        queryFn: () => {
-            return backendAPI.get(`/breeds`).then((response => {
-                return response.data
-            }))
-        },enabled:!!accessToken
-    })
+  const backendAPI = getAxiosBackend(accessToken);
+  return useQuery<Breed[]>({
+    queryKey: ["breeds"],
+    queryFn: () => {
+      return backendAPI.get(`/breeds`).then((response) => {
+        return response.data;
+      });
+    },
+    enabled: !!accessToken,
+  });
 }
 
 export function useGetBreedInfo(accessToken: string, breedId: number) {
@@ -77,74 +75,17 @@ export const useUploadDogPhoto = (accessToken: string) => {
 };
 
 export const useGetDogPhoto = (accessToken: string, id: number) => {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useQuery<string>({
-      queryKey: ["getDogPhoto", id],
-      queryFn: () => {
-        return backendAPI.get(`/photos/${id}`).then((response) => {
-          return response.data;
-        });
-      },
-      enabled: !!accessToken,
-    });
-  };  
-
-export function useCreateProfile(accessToken: string) {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useMutation((dogProfile: DogProfile) => {
-        return backendAPI.post(`/dogs/profiles`, dogProfile)
-    });
+  const backendAPI = getAxiosBackend(accessToken);
+  return useQuery<string>({
+    queryKey: ["getDogPhoto", id],
+    queryFn: () => {
+      return backendAPI.get(`/photos/${id}`).then((response) => {
+        return response.data;
+      });
+    },
+    enabled: !!accessToken,
+  });
 };
-
-export function useGetDogProfileByDogId(accessToken: string, dogId: number) {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useQuery<DogProfile>({
-        queryKey: ["getDogProfileByDogId", dogId],
-        queryFn: () => {
-            return backendAPI.get(`dogs/profiles/dog/${dogId}`).then((response) => {
-                return response.data;
-            })
-        },
-        enabled: !!accessToken,
-    })
-}
-
-export function useGetDogOwnersByDogId(accessToken: string, dogId: number) {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useQuery<DogOwner[]>({
-        queryKey: ["getDogProfileByDogId", dogId],
-        queryFn: () => {
-            return backendAPI.get(`dogs/${dogId}/owners`).then((response) => {
-                return response.data;
-            })
-        },
-        enabled: !!accessToken,
-    })
-}
-
-export function useDeleteDogProfile(accessToken: string) {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useMutation({
-        mutationFn: (id: DogProfile["id"]) => {
-            return backendAPI.delete(`dogs/profiles/${id}`).then((response) => {
-                response.data;
-            })
-        }
-    })
-    
-}
-
-export function useUpdateDogProfile(accessToken: string) {
-    const backendAPI = getAxiosBackend(accessToken);
-    return useMutation({
-        mutationFn: (dogProfile: DogProfile) => {
-            return backendAPI.put(`dogs/profiles/${dogProfile.id}`, dogProfile).then(((response) => { response.data;
-             }))
-        }
-    }) 
-}
-
-
 
 export function useCreateProfile(accessToken: string) {
   const backendAPI = getAxiosBackend(accessToken);
