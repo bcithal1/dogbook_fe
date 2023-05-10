055b2067558396f47fbcd3e7b31a3ef565ba694d
Littlezoski <shicheng.zhou@my.utsa.edu>
2023-05-09 20:22:54 -0500

    add an event map page

---
+++ src/pages/map.tsx
@@ -0,0 +1,12 @@
+import EventMap from '@/components/map/EventMap'
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+function map() {
+  return (
+    <Flex justifyContent={"center"} ><EventMap /></Flex>
+    
+  )
+}
+
+export default map
