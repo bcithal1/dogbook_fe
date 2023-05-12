a015470a46a77617119007981b281099c3b5a7b7
Brian <bcithal1@gmail.com>
2023-05-12 13:53:09 -0500

    Included the code that handles tagging

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,23 +1,52 @@
 import { Box, Button, Textarea } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import { useSession } from "next-auth/react";
 import { useCreatePost } from "@/queries/post.queries";
 import { Post } from "@/types/post";
 import { FaPaw } from "react-icons/fa";
+import { useGetFriendList } from "@/queries/friend.queries";
 
 function CreatePostForm() {
   const { data: session } = useSession();
+  const userFriends = useGetFriendList(session?.accessToken, "2");
   const createPostMutation = useCreatePost(session?.accessToken);
   const [message, setMessage] = useState<Post["message"] | null>(null);
-  session.user?.id;
+  const [searchTerm, setSearchTerm] = useState<string | null>(null);
+  const [isTagging, setIsTagging] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (searchTerm) {
+      console.log(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value as Post["message"];
+    setMessage(value);
+
+    if (value.charAt(value.length - 1) === "@") {
+      // if the last character is '@', set the searchTerm state to be empty and setIsTagging to true
+      setSearchTerm("");
+      setIsTagging(true);
+    } else if (isTagging) {
+      if (value.charAt(value.length - 1) === " ") {
+        // if the last character is ' ', set the setIsTagging to false
+        setIsTagging(false);
+      } else {
+        // if isTagging is true, append the new character to searchTerm
+        setSearchTerm(
+          (prevSearchTerm) => prevSearchTerm + value.charAt(value.length - 1)
+        );
+      }
+    }
+  };
 
   async function handleClick() {
     const post: Post = {
       message,
     };
 
-    console.log(post);
     try {
       createPostMutation.mutateAsync(post);
     } catch {}
