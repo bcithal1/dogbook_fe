2d53ef494015b355c1195a567c7282e1aa964b86
iustina.istrati@mutualofomaha.com <iustina.istrati@mutualofomaha.com>
2023-05-20 10:31:24 -0500

    committin

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -45,7 +45,7 @@ export default function notifications() {
           gap="9"
         >
           <Box
-            w="360px"
+            w="460px"
             overflowY="auto"
             maxHeight="450px"
             borderRadius={10}
@@ -74,7 +74,7 @@ export default function notifications() {
           </Box>
 
           <Box
-            w="360px"
+            w="460px"
             rounded="sm"
             borderRadius={10}
             my={5}
@@ -93,13 +93,36 @@ export default function notifications() {
             </Center>
             <Box h="400px" borderBottom="1px" width="100%">
               {/* import friend requests here */}
-              <FriendNotification />
+              {/* <FriendNotification /> */}
             </Box>
-            <PostNotification
-              user={userData}
-              userProfile={userProfile}
-              session={session}
-            />
+          </Box>
+
+          <Box
+            w="460px"
+            overflowY="auto"
+            maxHeight="450px"
+            borderRadius={10}
+            my={5}
+            mx={[0, 5]}
+            bg="#886E58"
+            boxShadow={
+              "0px 1px 25px -5px rgb(0 0 0 / 57%), 0 10px 10px -5px rgb(0 0 0 / 45%)"
+            }
+          >
+            {/* event */}
+            <Center mt="20px">
+              <Text fontSize={"xl"} fontWeight="medium" color={"#ffffff"}>
+                Posts
+              </Text>
+            </Center>
+            <Box h="400px" borderBottom="1px" width="97%">
+              {/* import event list here */}
+              <PostNotification
+                user={userData}
+                userProfile={userProfile}
+                session={session}
+              />
+            </Box>
           </Box>
         </Flex>
       </Flex>
